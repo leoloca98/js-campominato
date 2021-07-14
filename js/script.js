d12bc624902ea9a26f4dbc7e778e90b6a5b11b98
leoloca98 <leonardolocatelli98@gmail.com>
2021-07-14 16:09:53 +0200

    Add all comments

--- js/script.js
+++ js/script.js
@@ -28,7 +28,7 @@ Quando la partita termina, comunichiamo all'utente il suo punteggio.
 // 6 - Se non è presente neanche in questo pushare il numero nell'array
 // 7 - Far ricominciare il gioco dal punto //#1
 
-// |COLLEGAMENTO HTML
+// |FORM
 const userNumField = document.getElementById("user-number");
 const selectDiff = document.getElementById("select-difficulty");
 const diffBtn = document.getElementById("difficulty-btn");
@@ -37,8 +37,10 @@ const youLose = document.getElementById("game-over");
 const result = document.getElementById("result");
 const counter = document.getElementById("counter");
 const numMessage = document.getElementById("message-number");
+const numberRight = document.getElementById("number-right");
+const resetBtn = document.getElementById("reset-btn");
 
-// |SEZIONI
+// |SECTION
 const gameDiffSection = document.getElementById("game-difficulty");
 const numberSection = document.getElementById("number");
 const buttonVerifySection = document.getElementById("button-verify");
@@ -46,67 +48,91 @@ const counterSection = document.getElementById("increase");
 const endGameSection = document.getElementById("end-game");
 
 
-// |DICHIARAZIONE VARIABILI
+// |ELEMENTS    
 
-let bombsContainer = [];                                                                        // Array per le bomber
-let userNumContainer = [];                                                                      // Array per i numeri dell'utente
-let randomNum = 0;
-let userNum = 0;                                                                                // Conterrà i numeri generati random per le bombe
-let difficulty = 100;                                                                           // Verrà usata come flag per il GameOver                                                                       // Verrà usata come flag per il GameOver
-let gameOver = false;
-let counterNum = 0;                                                                             // Verrà usata per scegliere la difficoltà
+let bombsContainer = [];                                                                            // Array per le bomber
+let userNumContainer = [];                                                                          // Array per i numeri dell'utente
+let randomNum = 0;                                                                                  // Conterrà i numeri generati random per le bombe
+let userNum = 0;                                                                                    // Numero scelto dall'utente
+let difficulty = 100;                                                                               // Verrà usata per scegliere la difficoltà
+let gameOver = false;                                                                               // Verrà usata come flag per il GameOver 
 
 // |PREPARAZIONE
-numberSection.classList.add("d-none");
-buttonVerifySection.classList.add("d-none");
+numberSection.classList.add("d-none");                                                              // Rimuovo dal flusso la sezione NUMBER
+buttonVerifySection.classList.add("d-none");                                                        // Rimuovo dal flusso la sezione BUTTON
+endGameSection.classList.add("d-none");                                                             // Rimuovo dal flusso la sezione END-GAME
 
 // |GIOCO
 
 // #CLICK DIFFICULTY
-diffBtn.addEventListener("click", function () {
+diffBtn.addEventListener("click", function () {                                                     // Alla pressione del button SELECT DIFFICULTY...
 
     // |SELEZIONARE DIFFICOLTA'
-    difficulty = selectDiff.value;                                                              // Imposta la difficoltà del gioco
+    difficulty = selectDiff.value;                                                                  // Imposta la difficoltà del gioco
 
     // |INSERIMENTO DEI NUMERI BOMBA NELL'ARRAY IN BASE ALLA DIFFICOLTA' (NO DOPPIONI)
-    while (bombsContainer.length < 16) {                                                        // Se il vettore non ha raggiunto il 16 "posti occupati"
-        randomNum = Math.floor(Math.random() * (difficulty) + 1);                                  // Genero un numero random da 1 a 100 (compresi)
-        if (!bombsContainer.includes(randomNum)) {                                              // Se il numero non è incluso nell'array bombe
-            bombsContainer.push(randomNum);                                                     // Allora lo pusho al suo interno
+    while (bombsContainer.length < 16) {                                                            // Se il vettore non ha raggiunto il 16 "posti occupati"
+        randomNum = Math.floor(Math.random() * (difficulty) + 1);                                   // Genero un numero random da 1 a 100 (compresi)
+        if (!bombsContainer.includes(randomNum)) {                                                  // Se il numero non è incluso nell'array bombe
+            bombsContainer.push(randomNum);                                                         // Allora lo pusho al suo interno
         }
     }
-    console.table(bombsContainer);
+    console.table(bombsContainer);                                                                  // Stampo in console la tabella delle posizioni delle bombe
 
-    gameDiffSection.classList.add("d-none");
-    numberSection.classList.remove("d-none");
-    buttonVerifySection.classList.remove("d-none");
-    counterSection.classList.remove("d-none");
-    endGameSection.classList.remove("d-none");
+    gameDiffSection.classList.add("d-none");                                                        // Rimuovo dal flusso la sezione DIFFICULTY
+    numberSection.classList.remove("d-none");                                                       // Rimetto nel flusso la sezione NUMBER
+    buttonVerifySection.classList.remove("d-none");                                                 // Rimetto nel flusso la sezione BUTTON
+    counterSection.classList.remove("d-none");                                                      // Rimetto nel flusso la sezione COUNTER
 
-    numMessage.innerHTML = "Inserire un numero da 1 a " + difficulty;
+
+    numMessage.innerHTML = "Inserire un numero da 1 a " + difficulty;                               // Messaggio che anticipa l'input del numero (in base alla difficoltà)
 
 });
 
 
 // #CLICK VERIFY   
-//TODO alert "numero gia inserito"
-verifyBtn.addEventListener("click", function () {
-    userNum = parseInt(userNumField.value);                                                     // Numero inserito dall'utente                                                                       
 
-    if (gameOver == false && userNum >= 1 && userNum <= difficulty) {                           // Controllo se è compreso tra 1 e 100 e se la flag è ancora su false
-        if (bombsContainer.includes(userNum)) {                                                 // Se il numero inserito è gia nell'array delle bombe...    
-            //|GAME OVER
-            gameOver = true;                                                                    // Imposto la flag su true
-            youLose.innerHTML = "GAME OVER";                                                    // Stampo il messaggio di sconfitta
-            result.innerHTML = "Hai ottenuto il punteggio di: " + userNumContainer.length;      // Stampo il punteggio ottenuto dall'utente                                                   
+verifyBtn.addEventListener("click", function () {                                                   // Alla pressione del button VERIFY...
+    userNum = parseInt(userNumField.value);                                                         // Numero inserito dall'utente                                                                       
 
-        }
-        if (!userNumContainer.includes(userNum)) {                                         // Se il numero non è presente nell'array dei numeri scelti                                               
-            userNumContainer.push(userNum);                                                     // Pusho al suo interno il numero
-            userNumField.value = "";                                                            // Azzero il campo dei numeri
-        }
-        else {
-            alert("Numero già inserito");
-        }
+    if (gameOver == false) {                                                                        // Controllo se la flag è stata beccata una bomba
+        if (userNum >= 1 && userNum <= difficulty) {                                                // Controllo se è compreso tra 1 e difficulty
+            if (bombsContainer.includes(userNum)) {                                                 // Se il numero inserito è gia nell'array delle bombe...    
+                //|GAME OVER
+                gameOver = true;                                                                    // Imposto la flag su true
+
+                endGameSection.classList.remove("d-none");                                          // Rimetto nel flusso la sezione END-GAME
+                numberSection.classList.add("d-none");                                              // Tolgo dal flusso la sezione NUMBER
+                buttonVerifySection.classList.add("d-none");                                        // Tolgo dal flusso la sezione BUTTON
+
+                youLose.innerHTML = "GAME OVER";                                                    // Stampo il messaggio di sconfitta
+                result.innerHTML = "Hai ottenuto il punteggio di: " + userNumContainer.length;      // Stampo il punteggio ottenuto dall'utente 
+                numberRight.innerHTML = "Hai indovinato i seguenti numeri: " + userNumContainer;    // Stampo tutti i numeri che ha indovinato l'utente
+
+            }
+            if (!userNumContainer.includes(userNum)) {                                              // Se il numero non è presente nell'array dei numeri scelti                                               
+                userNumContainer.push(userNum);                                                     // Pusho al suo interno il numero
+                userNumField.value = "";                                                            // Azzero il campo dei numeri
+            }
+            else {
+                alert("Numero già inserito");                                                       // ALERT per numero gia inserito
+            }
+        } else alert("Inserire un numero da 1 a " + difficulty + " (compresi)");                    // ALERT per numero non compreso in 1 e difficulty
+
     }
+});
+
+// #CLICK RESET
+
+resetBtn.addEventListener("click", function () {                                                    // Alla pressione del button RESET...
+
+    gameOver = false;                                                                               // Reiposto la flag su false
+    bombsContainer = [];                                                                            // Svuoto l'array delle bombe
+    userNumContainer = [];                                                                          // Svuoto l'array dei numeri scelti dall'utente
+
+    gameDiffSection.classList.remove("d-none");                                                     // Rimetto nel flusso la sezione DIFFICULTY
+    numberSection.classList.add("d-none");                                                          // Tolgo dal flusso la sezione NUMBER
+    buttonVerifySection.classList.add("d-none");                                                    // Tolgo dal flusso la sezione BUTTON
+    counterSection.classList.add("d-none");                                                         // Tolgo dal flusso la sezione COUNTER
+    endGameSection.classList.add("d-none");                                                         // Tolgo dal flusso la sezione END-GAME
 });
